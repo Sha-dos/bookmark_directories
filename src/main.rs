f1f05c7f75afb497aa25b6914fb3002df06843b0
Sha-dos <ndpete2007@gmail.com>
2022-10-21 17:00:04 -0500

    push

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::process::exit;
 use std::{env, fs, io};
 use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, Read};
@@ -13,41 +14,76 @@ struct SavedDir {
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 { eprintln!("Error: Expected 1 argument"); }
-    else {
-        let saved_path = format!("{}{}", env::home_dir().unwrap().to_str().unwrap(), "/.bookmarked_dirs");
-        // println!("{}", &saved_path);
+    if args.len() != 2 { eprintln!("Error: Expected 1 argument!"); }
 
-        if !Path::new(&saved_path).exists() {
-            File::create(&saved_path);
-        }
+    match &args[1].as_str() {
+        &"init" => {
+            let shell_path = format!("{}{}", env::home_dir().unwrap().to_str().unwrap(), "/cd.sh");
 
-        let lines = read_lines(PathBuf::from(&saved_path)).unwrap();
-
-        if fs::read_to_string(&saved_path).unwrap().contains(&args[1]) {
-            let mut saved_dirs: Vec<SavedDir> = Vec::new();
-
-            for line in lines {
-                saved_dirs.push(parse(&mut line.unwrap()));
+            if !Path::new(&shell_path).exists() {
+                File::create(&shell_path);
             }
 
-            // println!("Dirs: {:?}", saved_dirs[0]);
+            let mut file = OpenOptions::new()
+            .write(true)
+            .open(&shell_path)
+            .unwrap();
 
-            for saved_dir in saved_dirs {
-                if saved_dir.name == args[1] {
-                    goto_dir(saved_dir.dir);
-                }
-            }
-        } else {
+            let data = String::from("#!/bin/bash
+            output=$(bookmark_directories $1)
+            cd $output"
+            );
+
+            file.write(data.as_bytes());
+
+            let bashrc_path = format!("{}{}", env::home_dir().unwrap().to_str().unwrap(), "/.bashrc");
+
             let mut file = OpenOptions::new()
             .write(true)
             .append(true)
-            .open(&saved_path)
+            .open(bashrc_path)
             .unwrap();
 
-            let data = format!("{},{}", &args[1], env::current_dir().unwrap().to_str().unwrap());
+            file.write(br#"alias mrk=". ~/cd.sh""#);
+        }
 
-            file.write_all(data.as_bytes());
+        _ => {
+            let saved_path = format!("{}{}", env::home_dir().unwrap().to_str().unwrap(), "/.bookmarked_dirs");
+            // println!("{}", &saved_path);
+
+            if !Path::new(&saved_path).exists() {
+                File::create(&saved_path);
+            }
+
+            let lines = read_lines(PathBuf::from(&saved_path)).unwrap();
+
+            if fs::read_to_string(&saved_path).unwrap().contains(&args[1]) {
+                let mut saved_dirs: Vec<SavedDir> = Vec::new();
+
+                for line in lines {
+                    //println!("Data: {}", &line.unwrap());
+                    saved_dirs.push(parse(&mut line.unwrap()));
+                }
+
+                // println!("Dirs: {:?}", saved_dirs[0]);
+
+                for saved_dir in saved_dirs {
+                    if saved_dir.name == args[1] {
+                        goto_dir(saved_dir.dir);
+                    }
+                }
+            } else {
+                let mut file = OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open(&saved_path)
+                .unwrap();
+
+                let data = format!("{},{}", &args[1], env::current_dir().unwrap().to_str().unwrap());
+
+                //file.write_all(data.as_bytes());
+                writeln!(file, "{}", data);
+            }
         }
     }
 }
@@ -55,12 +91,12 @@ fn main() {
 fn parse(data: &mut String) -> SavedDir {
     let split: Vec<&str> = data.split(",").collect();
 
-    let name = split[0].to_string();
-    let dir = split[1].to_string();
+    let name = &split[0].to_string();
+    let dir = &split[1].to_string();
 
     SavedDir {
-        name,
-        dir,
+        name: name.to_string(),
+        dir: dir.to_string(),
     }
 }
 
